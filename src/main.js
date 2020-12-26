eb1a57edac32662a1d1a11f26b7119eddfea1972
lizhiqiang <13100711029@163@.com>
2020-12-27 00:30:26 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import vueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+// import elementUi from 'element-ui'
+
+createApp(App)
+    .use(vueAxios,axios)
+    // .use(elementUi)
+    .mount('#app')
+
